32345afa13807f552d120681699a41f4444e7a56
Elliot Olbright <elliotolbright@gmail.com>
2021-06-03 09:12:26 -0600

    add person_spec and 'exists' test

---
+++ spec/person_spec.rb
@@ -0,0 +1,12 @@
+require 'rspec'
+ require './lib/person'
+
+RSpec.describe Person do
+
+  it 'exists' do
+    kathy = Person.new('Kathy Chan', 10)
+
+    expect(kathy).to be_a(Person)
+  end
+  
+end
