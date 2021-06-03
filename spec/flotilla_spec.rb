d85ec7c6c35f138e16072795f68093ff84f22157
Elliot Olbright <elliotolbright@gmail.com>
2021-06-03 09:51:21 -0600

    add #add_personaell test

--- spec/flotilla_spec.rb
+++ spec/flotilla_spec.rb
@@ -33,4 +33,29 @@ RSpec.describe Flotilla do
     seventh_flotilla.add_ship(daedalus)
     expect(seventh_flotilla.ships).to eq([daedalus])
   end
+
+  it 'can add personnel' do 
+    daedalus = Spacecraft.new({name: 'Daedalus', fuel: 400})
+    seventh_flotilla = Flotilla.new({designation: 'Seventh Flotilla'})
+    kathy = Person.new('Kathy Chan', 10)
+    polly = Person.new('Polly Parker', 8)
+    kathy.add_specialty(:astrophysics)
+    kathy.add_specialty(:quantum_mechanics)
+    polly.add_specialty(:operations)
+    polly.add_specialty(:maintenance)
+    rover = Person.new('Rover Henriette', 1)
+    rover.add_specialty(:operations)
+    rover.add_specialty(:maintenance)
+    sampson = Person.new('Sampson Edwards', 7)
+    sampson.add_specialty(:astrophysics)
+    sampson.add_specialty(:quantum_mechanics)
+    seventh_flotilla.add_personnel(kathy)
+    seventh_flotilla.add_personnel(polly)
+    seventh_flotilla.add_personnel(rover)
+    seventh_flotilla.add_personnel(sampson)
+    seventh_flotilla.add_ship(daedalus)
+
+    expect(seventh_flotilla.ships).to eq([daedalus])
+    expect(seventh_flotilla.personnel).to eq(([kathy, polly, rover, sampson]))
+  end
 end
