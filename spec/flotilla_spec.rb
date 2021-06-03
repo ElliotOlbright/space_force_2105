368ba2b8cf5995aadaf86b1dcee1605a67c103d0
Elliot Olbright <elliotolbright@gmail.com>
2021-06-03 09:59:17 -0600

    add #reccomend_personell test

--- spec/flotilla_spec.rb
+++ spec/flotilla_spec.rb
@@ -58,4 +58,25 @@ RSpec.describe Flotilla do
     expect(seventh_flotilla.ships).to eq([daedalus])
     expect(seventh_flotilla.personnel).to eq(([kathy, polly, rover, sampson]))
   end
+
+  it 'can recommend personell' do 
+    daedalus = Spacecraft.new({name: 'Daedalus', fuel: 400})
+    daedalus.add_requirement({astrophysics: 6})
+    daedalus.add_requirement({quantum_mechanics: 3})
+    seventh_flotilla = Flotilla.new({designation: 'Seventh Flotilla'})
+    kathy = Person.new('Kathy Chan', 10)
+    polly = Person.new('Polly Parker', 8)
+    kathy.add_specialty(:astrophysics)
+    kathy.add_specialty(:quantum_mechanics)
+    polly.add_specialty(:operations)
+    polly.add_specialty(:maintenance)
+    rover = Person.new('Rover Henriette', 1)
+    rover.add_specialty(:operations)
+    rover.add_specialty(:maintenance)
+    sampson = Person.new('Sampson Edwards', 7)
+    sampson.add_specialty(:astrophysics)
+    sampson.add_specialty(:quantum_mechanics)
+
+    expect(seventh_flotilla.recommend_personnel(daedalus).to eq([kathy, sampson])
+  end 
 end
