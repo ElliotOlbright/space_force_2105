4e2fd1543ee578b2a27e7d0beffdbf0e7dd8eddc
Elliot Olbright <elliotolbright@gmail.com>
2021-06-03 09:36:08 -0600

    add 'it exists' test Flotilla

--- spec/flotilla_spec.rb
+++ spec/flotilla_spec.rb
@@ -1,13 +1,14 @@
-# require 'rspec'
-#  require './lib/flotilla'
-#  require './lib/person'
-#  require './lib/spacecraft'
+require 'rspec'
+ require './lib/flotilla'
+ require './lib/person'
+ require './lib/spacecraft'
 
-# RSpec.describe Flotilla do
+RSpec.describe Flotilla do
 
-#   it 'exists' do
-#     daedalus = Spacecraft.new({name: 'Daedalus', fuel: 400})
+  it 'exists' do
+    daedalus = Spacecraft.new({name: 'Daedalus', fuel: 400})
+    seventh_flotilla = Flotilla.new({designation: 'Seventh Flotilla'})
 
-
-#   end
-# end
+    expect(seventh_flotilla).to be_a(Flotilla)
+  end
+end
