0b086d557058e9e1d9b5ff6a79bb817932949fd1
Elliot Olbright <elliotolbright@gmail.com>
2021-06-03 09:34:20 -0600

    add requirements test

--- lib/spacecraft.rb
+++ lib/spacecraft.rb
@@ -1,8 +1,10 @@
 class Spacecraft
-  attr_reader :name, :fuel
+  attr_reader :name, :fuel, :requirements
   def initialize(data)
     @name = data[:name]
     @fuel = data[:fuel]
-
+    @requirements = []
   end 
+
+
 end 
--- spec/flotilla_spec.rb
+++ spec/flotilla_spec.rb
@@ -1,14 +1,13 @@
-require 'rspec'
- require './lib/flotilla'
- require './lib/person'
- require './lib/spacecraft'
+# require 'rspec'
+#  require './lib/flotilla'
+#  require './lib/person'
+#  require './lib/spacecraft'
 
-RSpec.describe Flotilla do
+# RSpec.describe Flotilla do
 
-    it 'exists' do
-      daedalus = Spacecraft.new({name: 'Daedalus', fuel: 400})
+#   it 'exists' do
+#     daedalus = Spacecraft.new({name: 'Daedalus', fuel: 400})
 
 
-    end
-  end
-end
+#   end
+# end
